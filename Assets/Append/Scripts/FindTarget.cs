1f19106fa0fd19effb2bcdb850c0f06752d7379f
teramotodaiki <i101323@gmail.com>
2014-04-20 16:13:48 +0900

    一定距離まで近付くと、近付いてくるコンポーネント
    動かしたいオブジェクトにWalkTowardBehaviourをアタッチし、Tag NameにPlayerなど標的のタグ名をセットします。
    そのオブジェクトの子要素に、新しいGameObjectを追加します。そして、そこにFindTargetをアタッチします。Find
    TagにはPlayerなど近付いたとき反応するタグ名をセットします。
    子要素の方にSphere Colliderなどのコライダをアタッチします。このコライダにPlayerなどが触れると、ついてくるようになります。
    
    ICallable.csも必要です！

---
+++ Assets/Append/Scripts/FindTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class FindTarget : MonoBehaviour {
+
+	[TagNameAttribute]
+	public string FindTag = "Player";
+
+	void Start()
+	{
+		collider.isTrigger = true;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == FindTag && transform.parent) {
+			foreach (var item in transform.parent.GetComponents<Component>()) {
+				if(item is ICallable)
+					(item as ICallable).Call();
+			}
+		}
+	}
+}
--- Assets/Append/Scripts/WalkTowardBehaviour.cs
+++ Assets/Append/Scripts/WalkTowardBehaviour.cs
@@ -2,28 +2,38 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
-public class WalkTowardBehaviour : MonoBehaviour {
+public class WalkTowardBehaviour : MonoBehaviour, ICallable {
 
 	[TagNameAttribute]
 	public string TagName = "Player";
 	public float Force = 1f;
 	[Range(0, 1)]
 	public float TurnSpeed = 0.1f;
+	public bool WalkOnStart = false;
 
 	private Transform target;
+	private bool awaken = false;
 
 	// Use this for initialization
 	void Start ()
 	{
 		target = GameObject.FindGameObjectWithTag (TagName).transform;
+		awaken = WalkOnStart;
 	}
 
 	void FixedUpdate()
 	{
-		var lookat = this.target.position - this.transform.position; // turn vector
-		lookat = new Vector3(lookat.x, this.transform.forward.y, lookat.z) ; // DON'T MOVE Y (Up or Down)
+		if (awaken) {
+			var lookat = this.target.position - this.transform.position; // turn vector
+			lookat = new Vector3 (lookat.x, this.transform.forward.y, lookat.z); // DON'T MOVE Y (Up or Down)
 
-		this.transform.forward = Vector3.Slerp (this.transform.forward, lookat, TurnSpeed); // slow turn
-		this.rigidbody.AddForce (this.transform.forward * this.Force); // walk
+			this.transform.forward = Vector3.Slerp (this.transform.forward, lookat, TurnSpeed); // slow turn
+			this.rigidbody.AddForce (this.transform.forward * this.Force); // walk
+		}
+	}
+
+	public void Call()
+	{
+		awaken = true;
 	}
 }
