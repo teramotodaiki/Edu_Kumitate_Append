265484d9027189835a8ce72c3f9ac51d8d5f429d
teramotodaiki <i101323@gmail.com>
2014-04-20 13:56:55 +0900

    Generaterを作成
    好きなPrefabを一定時間毎に生成するGenerater

---
+++ Assets/Append/Scripts/GeneraterBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneraterBehaviour : MonoBehaviour {
+	
+	public GameObject Prefab;
+	public float IntervalTime;
+
+	private float spendTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((spendTime += Time.deltaTime) >= this.IntervalTime){
+			this.spendTime = 0;
+			GameObject.Instantiate(this.Prefab, this.transform.position, Quaternion.identity);
+		}
+	}
+}
